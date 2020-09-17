bb938d840da4cc66fa781877c256c97836b5ef8f
goll <m.goll@gmail.com>
2020-09-17 17:17:44 +0200

    cors

--- Aramis.TrainMovementData.WebAPI/Startup.cs
+++ Aramis.TrainMovementData.WebAPI/Startup.cs
@@ -29,8 +29,13 @@ namespace Aramis.TrainMovementData.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
 
-            services.AddCors();
             DbConnectionStringBuilder connectionStringBuilder = new DbConnectionStringBuilder
             {
                 ConnectionString = Configuration.GetConnectionString("Default")
